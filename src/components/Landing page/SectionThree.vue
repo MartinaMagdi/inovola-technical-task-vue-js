3d5e123c0faa9c8a6220439e7d57f2ee381ade15
Martina Magdi <martinamagdi6@gmail.com>
2021-12-27 03:52:18 +0200

    Finishing section three in landing page

---
+++ src/components/Landing page/SectionThree.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="section-three">
+    <p class="first-text">التطبيقات المميزة</p>
+    <p class="second-text">تطبيقات تقدم حلول وخدمات مميزة نوصي بتجربتها</p>
+
+    <div class="features-apps-container">
+      <div class="item" v-for="item in featuredAppsList" :key="item.id">
+        <div class="right-side">
+          <img :src="item.img" :alt="item.altImg" />
+        </div>
+        <div class="left-side">
+          <div class="title-container">
+            <p class="card-title">{{ item.name }}</p>
+            <span><v-icon>mdi-star</v-icon> {{ item.starNum }}</span>
+          </div>
+          <p class="developer-name">
+            تطوير: <span>{{ item.developerName }}</span>
+          </p>
+          <p class="description">{{ item.firstDes }}</p>
+          <p class="description">{{ item.secondDes }}</p>
+        </div>
+      </div>
+    </div>
+
+    <button class="btn">
+      <span>تصفح التطبيقات</span>
+      <v-icon>mdi-chevron-left</v-icon>
+    </button>
+  </div>
+</template>
+
+<script>
+import cardImage from "../../assets/images/Landing page/section3-img.png";
+
+export default {
+  name: "SectionThree",
+  data: () => ({
+    featuredAppsList: [
+      {
+        id: 0,
+        img: cardImage,
+        altImg: "logo",
+        name: "قمرة",
+        starNum: 5,
+        developerName: "عمر برهوم",
+        firstDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+        secondDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+      },
+      {
+        id: 1,
+        img: cardImage,
+        altImg: "logo",
+        name: "قمرة",
+        starNum: 5,
+        developerName: "عمر برهوم",
+        firstDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+        secondDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+      },
+      {
+        id: 2,
+        img: cardImage,
+        altImg: "logo",
+        name: "قمرة",
+        starNum: 5,
+        developerName: "عمر برهوم",
+        firstDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+        secondDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+      },
+      {
+        id: 3,
+        img: cardImage,
+        altImg: "logo",
+        name: "قمرة",
+        starNum: 5,
+        developerName: "عمر برهوم",
+        firstDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+        secondDes: "نظام الولاء الأمثل الذي يمكنك من رفع مبيعاتك",
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/variables";
+@import "../../assets/styles/Landing page/SectionThree";
+</style>
--- src/components/Landing page/index.vue
+++ src/components/Landing page/index.vue
@@ -2,15 +2,17 @@
   <div class="landing-page">
     <SectionOne />
     <SectionTwo />
+    <SectionThree />
   </div>
 </template>
 
 <script>
 import SectionOne from "./SectionOne.vue";
 import SectionTwo from "./SectionTwo.vue";
+import SectionThree from "./SectionThree.vue";
 
 export default {
   name: "LandingPage",
-  components: { SectionOne, SectionTwo },
+  components: { SectionOne, SectionTwo, SectionThree },
 };
 </script>
